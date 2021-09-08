6fb91e94a201ae2e59d80fcb21f818c67ea219c7
javaravikiran <ravikiran.namana@gmail.com>
2021-09-08 16:05:33 +0530

    initial commit

---
+++ src/main/java/com/javaravi/controller/JenkinsDemoController.java
@@ -0,0 +1,17 @@
+package com.javaravi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jenkins")
+public class JenkinsDemoController {
+	
+	@GetMapping("/demo")
+	public String getUserDetails(@RequestParam("name") String name) {
+		return "Hi "+name+" Jenkins CI&CD Demo says Welcome To You!";
+	}
+
+}
